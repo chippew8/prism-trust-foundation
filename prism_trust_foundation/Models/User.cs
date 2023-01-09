145dcb2527985f9847f29278df781e962819273a
dclf000 <106300322+dclf000@users.noreply.github.com>
2023-01-09 17:13:01 +0800

    Login And Register

---
+++ prism_trust_foundation/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prism_trust_foundation.Models
+{
+    public class User
+    {
+
+        [Required]
+        [Key]
+        public string NRIC { get; set; }
+
+        [Required, MaxLength(25)]
+        public string Fname { get; set; }
+        [Required, MaxLength(25)]
+        public string Lname { get; set; }
+        [Required, MaxLength(50)]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+        public string PhoneNum { get; set; }
+        public string Role { get; set; }
+        public int PostalCode { get; set; }
+        [Required]
+        public string UnitNumber { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        public string Specialisation { get; set; }
+        public string HospitalAddress { get; set; }
+
+
+    }
+}
---
+++ prism_trust_foundation/Services/UserService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using prism_trust_foundation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace prism_trust_foundation.Services
+{
+    public class UserService
+    {
+            
+        private MyDbContext _context;
+        public UserService(MyDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public bool AddUser(User newuser)
+        {
+            if (UserExists(newuser.NRIC))
+            {
+                return false;
+            }
+            _context.Add(newuser);
+            _context.SaveChanges();
+            return true;
+        }
+        private bool UserExists(string id)
+        {
+            return _context.Users.Any(e => e.NRIC == id);
+        }
+
+    }
+}
