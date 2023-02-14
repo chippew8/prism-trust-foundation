191c62b03af642c0752dea03530930ee539633b9
Shrooded <oscaryong5678@gmail.com>
2023-02-15 04:33:14 +0800

    query index page

---
+++ prism_trust_foundation/Pages/Query/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace prism_trust_foundation.Pages.Query
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
