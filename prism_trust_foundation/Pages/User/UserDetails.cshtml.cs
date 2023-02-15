4381492231dc95606d401c6250ec125e8d768a83
Shrooded <oscaryong5678@gmail.com>
2023-02-15 10:27:45 +0800

    Editted admin layout

--- prism_trust_foundation/Pages/User/UserDetails.cshtml.cs
+++ prism_trust_foundation/Pages/User/UserDetails.cshtml.cs
@@ -1,6 +1,8 @@
+using EDP_Project.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using prism_trust_foundation.Models;
 using prism_trust_foundation.Services;
 using System;
@@ -14,20 +16,24 @@ namespace prism_trust_foundation.Pages.User
         private readonly IHttpContextAccessor contxt;
 
         private UserService _svc;
+        private readonly AuthDbContext _context;
 
-        public UserDetailsModel(SignInManager<ApplicationUser> signInManager, UserService service, IHttpContextAccessor httpContextAccessor)
+        public UserDetailsModel(SignInManager<ApplicationUser> signInManager, UserService service, IHttpContextAccessor httpContextAccessor, AuthDbContext context)
         {
             this.signinManager = signInManager;
             _svc = service;
             contxt = httpContextAccessor;
+            _context = context;
         }
 
         [BindProperty]
         public ApplicationUser HomeUser { get; set; } = new();
+        public List<Coupon> MyCoupons { get; set; }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGetAsync(string id)
         {
             string Email = contxt.HttpContext.Session.GetString("Email");
+            MyCoupons = _context.Users.Include(x => x.Coupon).FirstOrDefault(x => x.UserName == User.Identity.Name).Coupon.ToList();
             ApplicationUser? user = _svc.GetUserByEmail(Email);
             if (user != null)
             {
